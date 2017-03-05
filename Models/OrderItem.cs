0db5a8f9d59c2a2abde49316bff3d7dcbf4daa7a
wanderley.vega <leyveda@gmail.com>
2017-03-05 13:25:47 -0500

    nuevo

--- Models/OrderItem.cs
+++ Models/OrderItem.cs
@@ -1,12 +1,14 @@
+using Dapper.Contrib.Extensions;
+
 namespace Tibox.Models
 {
+    [Table("[OrderItem]")]
     public class OrderItem
     {
         public int Id { get; set; }
         public int OrderId { get; set; }
         public int ProductId { get; set; }
         public decimal UnitPrice { get; set; }
-        public int Quantity { get; set; }
-        public virtual Order Order { get; set; }        
+        public int Quantity { get; set; }   
     }
 }
